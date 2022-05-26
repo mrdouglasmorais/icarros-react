a4c2a0be2e7e7e7cb07dcd602ac31f0897cacb96
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2022-05-26 19:44:32 -0300

    finaly webpack production

---
+++ webpack.prod.js
@@ -0,0 +1,52 @@
+const { DefinePlugin } = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
+const common = require('./webpack.common');
+const { merge } = require('webpack-merge');
+
+module.exports = merge( common, {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js(x?)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }, {
+        test: /\.(scss|sass)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          }, {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }, {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  },
+  externals: {
+    react: 'React',
+    axios: 'axios',
+    'react-dom': 'ReactDOM',
+    'react-router-dom': 'ReactRouterDOM', 
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './template.prod.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main-bundle-[hash].css'
+    }),
+    new FaviconsWebpackPlugin({
+      logo: './public/favicon.jpeg'
+    })
+  ]
+})
