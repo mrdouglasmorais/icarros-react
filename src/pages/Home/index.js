51c0c60e8c78e2a7f983a3dbf701eff03d727e1a
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2022-05-26 21:45:54 -0300

    state react

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Nav from '@/components/Nav';
 import Footer from '@/components/Footer';
@@ -6,11 +6,28 @@ import Footer from '@/components/Footer';
 import { HomeSection } from './style'
 
 const Home = () => {
+
+  const [ isLoad, setIsLoad ] = useState(false)
+
+  const Test = () => {
+    setIsLoad(!isLoad);
+  } 
+  
   return(
     <>
       <Nav />
         <HomeSection>
           <h1>Home</h1>
+          { isLoad ? (
+            <>
+              <p>Carregando</p>
+            </>
+          ) : (
+            <>
+              <p>Pronto</p>
+            </>
+          )}
+          <button onClick={ Test }>Clique me</button>
         </HomeSection>
       <Footer />
     </>
